ac3a5978442605deff68e7343f827d2ddb12e24d
Софья Пак <alice-bo4kovsky@ya.ru>
2023-12-08 04:48:09 +0700

    структура консольного меню

--- lab5/Program.cs
+++ lab5/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+bool flag1 = true;
+bool flag2 = true;
+while (flag1) {
+    Console.WriteLine("Введите А");
+    Console.WriteLine("Введите B");
+    while(flag2)
+    {
+        Console.WriteLine("Меню операций:");
+        Console.WriteLine("1. Сложение A+B");
+        Console.WriteLine("2. Вычитание A-B");
+        Console.WriteLine("3. Умножение A*B");
+        Console.WriteLine("4. Деление A/B");
+        flag1 = false;
+        flag2 = false;
+    }
+};
