4a11708578a652febe1917c76f41d90c72c133ea
Софья Пак <alice-bo4kovsky@ya.ru>
2023-12-08 06:22:11 +0700

    финальное приложение

--- lab5/Program.cs
+++ lab5/Program.cs
@@ -18,7 +18,7 @@ int umn (int A, int B)
 
 double del (int A, int B)
 {
-    return A / B;
+    return (double)A / (double)B;
 }
 
 bool flag1 = true;
@@ -28,9 +28,10 @@ while (flag1) {
     Console.WriteLine("Введите А");
     string A_str = Console.ReadLine();
     bool success_A = int.TryParse(A_str, out number);
+    int A;
     if (success_A)
     {
-        int A = int.Parse(A_str);
+        A = int.Parse(A_str);
     }
     else 
     {
@@ -40,9 +41,11 @@ while (flag1) {
     Console.WriteLine("Введите B");
     string B_str = Console.ReadLine();
     bool success_B = int.TryParse(B_str, out number);
+
+    int B;
     if (success_B)
     {
-        int B = int.Parse(B_str);
+        B = int.Parse(B_str);
     }
     else
     {
@@ -56,7 +59,43 @@ while (flag1) {
         Console.WriteLine("2. Вычитание A-B");
         Console.WriteLine("3. Умножение A*B");
         Console.WriteLine("4. Деление A/B");
-        flag1 = false;
-        flag2 = false;
+        Console.WriteLine("5. Выбрать другие числа");
+        Console.WriteLine("Выберите операцию");
+        string num_str = Console.ReadLine();
+        bool success_num = int.TryParse(num_str, out number);
+        if (success_num)
+        {
+            int num = int.Parse(num_str);
+            int c;
+            double c2;
+            switch (num)
+            {
+                case 1:
+                    c = sum(A, B);
+                    Console.WriteLine($"{A} + {B} = {c}");
+                    break;
+                case 2:
+                    c = minus(A, B);
+                    Console.WriteLine($"{A} - {B} = {c}");
+                    break;
+                case 3:
+                    c = umn(A, B);
+                    Console.WriteLine($"{A} * {B} = {c}");
+                    break;
+                case 4:
+                    c2= del(A,B);
+                    Console.WriteLine($"{A} / {B} = {c2, 0:0.0}");
+                    break;
+                case 5:
+                    flag2= false;
+                    break;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Выберите из предложенных операций");
+            continue;
+        }
+
     }
 }
